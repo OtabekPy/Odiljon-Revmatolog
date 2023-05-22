7a3dce830a11cb680b07e69a3a2b8169bbfcb951
Otabek <tursunovotabekkuva@gmail.com>
2023-05-22 11:39:17 +0500

    first commit

---
+++ mainApp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Bemor)
+admin.site.register(Tolov)
+admin.site.register(Xulosa)
+admin.site.register(Xona)
+admin.site.register(Joylashtirish)
